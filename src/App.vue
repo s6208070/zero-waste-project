719ace7119a5cbd886ddf7e6e3bed0139376024d
Mok Wattanasopon <59221703+s6208070@users.noreply.github.com>
2021-05-09 11:49:54 +0700

    Initial Commit 2

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar 
+    app dark color = "indigo darken-1" 
+    elevate-on-scroll
+    src = "https://img.freepik.com/free-vector/abstract-background-with-dots-blurry-shapes_23-2148705493.jpg?size=626&ext=jpg&ga=GA1.2.1842958395.1604448000"
+    >
+      <v-toolbar-title class = "font-weight-medium"> INTERASHTIVE DATA </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn outlined rounded color = "white--text">
+        <span>LOGOUT</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Table />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  import Table from '@/components/Table'
+  export default {
+    name: 'App',
+    components: { Table },
+  }
+</script>
---
+++ src/components/TestImport.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn color="success">text</v-btn>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
